503bc601798e82fe59757bfa1b7f16ac72e9bafb
goncalveshigino <goncalveshigino20@gmail.com>
2023-10-24 17:17:56 +0100

    Initial Commit

---
+++ CinenaSwiftui/CinenaSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinenaSwiftuiApp.swift
+//  CinenaSwiftui
+//
+//  Created by Goncalves Higino on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CinenaSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CinenaSwiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CinenaSwiftui
+//
+//  Created by Goncalves Higino on 24/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
